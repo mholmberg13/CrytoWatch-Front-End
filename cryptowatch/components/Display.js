91c0bb6b9a2345416fc81ee2fe065408d1a6d284
Scott Horan <scotthoran@Scotts-Mac-mini.local>
2020-04-01 21:49:58 -0400

    created display component

--- cryptowatch/components/Display.js
+++ cryptowatch/components/Display.js
@@ -8,7 +8,7 @@ class CurrencyInfo extends React.Component {
             <h3>Currency Info:</h3>
             <h4>{this.props.currency.name}</h4>
             <h6><span>Rank:</span> {this.props.currency.rank}</h6>
-            <h6><span>Price:</span> {this.props.currency.price_usd}</h6>
+            <h6><span>Price:</span> {this.props.currency.price}</h6>
             <h6><span>Symbol:</span>{this.props.currency.symbol}</h6>
        </div>
       </>
