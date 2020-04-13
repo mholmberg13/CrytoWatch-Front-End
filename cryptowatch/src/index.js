6f147541a2ff50ca3f56ef94908b88d50ed7a9dc
Matt Holmberg <mattholmberg@Matts-MacBook-Pro.local>
2020-04-13 15:29:37 -0500

    Deploy takeoff

--- cryptowatch/src/index.js
+++ cryptowatch/src/index.js
@@ -4,6 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const path = require('path')
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
